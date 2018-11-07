e927f4467a2f242ff6bb14bcf5e24cce70f6e7cb
DESKTOP-Q1TEPBS\Countefeit <vitali.nizhnik@gmail.com>
2018-11-08 00:55:31 +0300

    Updated Task4.go
    Добавлена многопоточность

--- Task4/Task4.go
+++ Task4/Task4.go
@@ -11,15 +11,25 @@ func main(){
 	defer fmt.Println("World")
 		  fmt.Println("Hello")
 	}()
+
+	wg:= sync.WaitGroup{}
+
 	//задание 4.1**
+	sayHello := func(){
+		defer fmt.Println("World")
+		fmt.Println("Hello")
+
+	}
+
+	wg.Add(10)
 	once:= sync.Once{}
 	for i:=0; i<10; i++{
-		once.Do(sayHello)
+		go func(){
+			once.Do(sayHello)
+		}()
+		wg.Done()
 	}
-	fmt.Scanf(" ")
-}
+	wg.Wait()
 
-func sayHello(){
-	defer fmt.Println("World")
-	      fmt.Println("Hello")
+
 }
